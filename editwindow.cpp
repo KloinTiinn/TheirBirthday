15fcb6bec7f1fe9ed55d8f354c81eafc7d0e9067
DarkHobbit <pub@zvyozdochkin.ru>
2021-09-30 21:13:40 +0300

    GPL2+ License, desktop file and binaryimagebuildscript added

--- editwindow.cpp
+++ editwindow.cpp
@@ -1,3 +1,11 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version. See COPYING file for more details.
+ *
+ */
+
 #include "editwindow.h"
 #include "ui_editwindow.h"
 
